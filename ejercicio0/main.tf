d18b75ba01dbdf61d5a4b27647d83ade23ba27f3
Aisha del Tio <adeltiodeprado@gmail.com>
2026-03-06 19:58:37 +0100

    terraform

--- ejercicio0/main.tf
+++ ejercicio0/main.tf
@@ -1,1 +1,26 @@
-terraform --version
+
+resource "local_file" "pet" {
+    filename= var.filenameD
+    content= var.contentD
+    file_permission= "0644"
+}
+
+resource "local_sensitive_file" "personas" {
+    filename=var.filenameP
+    content= var.contentP
+}
+
+resource "random_pet" "server" {
+    prefix= "child"
+    length= 1
+    separator = "-"
+}
+
+resource "random_shuffle" "a" {
+    input= ["saul", "noelia", "fran"]
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = var.password_change
+}
---
+++ ejercicio0/variables.tf
@@ -0,0 +1,21 @@
+variable "filenameD" {
+    default = "dogs.txt"
+    type = string
+}
+variable "contentD" {
+    default = "Neva y Tortuga"
+    type = string   
+}
+variable "filenameP" {
+    default = "personas.txt"
+    type = string
+}
+variable "contentP" {
+    default = "Aisha y María"
+    type = string
+}
+
+variable "password_change" {
+    default = "true"
+    type = bool
+}
